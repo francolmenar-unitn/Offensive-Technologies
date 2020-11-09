dde4142b7e53f590cfdc2169d612dbe91bf4cee2
francolmenar <francolmenar@outlook.es>
2020-11-09 16:05:10 +0100

    Actual situation of the code, I have no idea what's going on

--- Snort/guide.sh
+++ Snort/guide.sh
@@ -4,9 +4,12 @@ ssh -tt otech2ae@users.deterlab.net "ssh -tt snort.snort-2bk.OffTech 'sudo -s'"
 ssh -tt otech2ae@users.deterlab.net "ssh -tt client1.snort-2bk.OffTech 'sudo -s'"
 ssh -tt otech2ae@users.deterlab.net "ssh -tt client2.snort-2bk.OffTech 'sudo -s'"
 ssh -tt otech2ae@users.deterlab.net "ssh -tt outsider.snort-2bk.OffTech 'sudo -s'"
+ssh -tt otech2ae@users.deterlab.net "ssh -tt server.snort-2bk.OffTech 'sudo -s'"
 
 
 ###################### SFTP login and Sending the Scripts ######################
 sftp otech2ae@users.deterlab.net
 
 put -r /Users/fran/Documents/Estudios/Master/2º-Master/Semester1/Offensive/Offensive-Technologies/Snort/scripts
+
+get /users/otech2ae/snort.conf /Users/fran/Documents/Estudios/Master/2º-Master/Semester1/Offensive/Offensive-Technologies/Snort/scripts/guard/
--- Snort/scripts/guard/guard.sh
+++ Snort/scripts/guard/guard.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+# /usr/local/snort-2.9.2.2/etc
+# /usr/local/snort-2.9.2.2/etc/snort.conf
+# /usr/local/snort-2.9.2.2/etc
+
+mkdir /usr/local/snort-2.9.2.2/rules
+touch /usr/local/snort-2.9.2.2/local.rules
+
 mkdir alerts
 
 sudo snort --daq nfq -Q -c snort.config -l alerts
