66ad37b376ba28d8f9325182d9a7d81628758a13
francolmenar <francolmenar@outlook.es>
2020-11-13 21:47:25 +0100

    log_analyze only reads the amount of requests and its difference

--- resilient_server/blue/server/log_analyze.sh
+++ resilient_server/blue/server/log_analyze.sh
@@ -6,8 +6,14 @@
 LOG_FOLDER=/var/log/apache2
 ACCESS_LOG=$LOG_FOLDER/access.log
 
+count_rows=0
 while sleep 1;do
-  count_rows=$(awk '{count++} END {print "count"}' $ACCESS_LOG)
+  count_aux=$(awk '{count++} END {print count}' $ACCESS_LOG) # Total amount of current rows
 
-  echo "The count of rows is $count_rows"
+  new_rows_count=$(expr $count_aux - $count_rows)  # Calculate the number of new rows
+  count_rows=$count_aux # Assign the actual number of rows to the count
+
+  echo "$count_rows amount of total rows"
+  echo "$new_rows_count new rows"
+  echo "----------------------------------"
 done
