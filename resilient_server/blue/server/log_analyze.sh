4e09838b3799f3df2241fa58ba528e15f295ac65
francolmenar <francolmenar@outlook.es>
2020-11-14 19:55:29 +0100

    Log analyzer for incomming requests done

--- resilient_server/blue/server/log_analyze.sh
+++ resilient_server/blue/server/log_analyze.sh
@@ -1,19 +1,22 @@
 #!/usr/bin/env bash
 
-# By https://edmondscommerce.github.io/bash/apache-log-file-analysis-script.html
-
-###### SETUP ############
+###### Setup Variables ############
 LOG_FOLDER=/var/log/apache2
 ACCESS_LOG=$LOG_FOLDER/access.log
 
-count_rows=0
+count_rows=0  # Initialize the counter
+
 while sleep 1;do
-  count_aux=$(awk '{count++} END {print count}' $ACCESS_LOG) # Total amount of current rows
+  count_aux=$(awk '{count++;} END {print count}' $ACCESS_LOG) # Total amount of current rows
 
   new_rows_count=$(expr $count_aux - $count_rows)  # Calculate the number of new rows
   count_rows=$count_aux # Assign the actual number of rows to the count
 
-  echo "$count_rows amount of total rows"
-  echo "$new_rows_count new rows"
-  echo "----------------------------------"
+  # Print the content of the new requests
+  rows_val=$(awk '{print "\t src: "$1 "\t\t content: "$6,$7,$8;}' $ACCESS_LOG | tail -"$new_rows_count" )
+
+  date | awk '{print $4}'
+  echo -e "\t\t$new_rows_count new requests"
+  echo -e "\t\t\t$rows_val"
+  echo "----------------------------------------------------------------------------------------------------------------------------"
 done
