8d1242dab4f4e2c855a4d95680dfd77414866098
Paolo Coba <paolo.coba@gmail.com>
2020-11-30 23:07:24 +0100

    Small change to exploiter

--- secure_server/red/legitimate.sh
+++ secure_server/red/legitimate.sh
@@ -5,13 +5,16 @@ if [ "$#" -ne 1 ];then
     exit 0
 fi
 
-curl -s -w 'Lookup time:\t%{time_total}'\\n -o /dev/null "$1/process.php?user=batman&pass=dummy&drop=register" &
+USER=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 10 | head -n 1)
+PASS=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 15 | head -n 1)
+
+curl -s -w 'Lookup time:\t%{time_total}'\\n -o /dev/null "$1/process.php?user=$USER&pass=$PASS&drop=register" &
 sleep 1
 
 while :
 do
-    curl -s -w 'Lookup time:\t%{time_total}'\\n -o /dev/null "$1/process.php?user=batman&pass=dummy&amount=2147483647&drop=deposit" &
+    curl -s -w 'Lookup time:\t%{time_total}'\\n -o /dev/null "$1/process.php?user=$USER&pass=$PASS&amount=2147483647&drop=deposit" &
     sleep 1
-    curl -s -w 'Lookup time:\t%{time_total}'\\n -o /dev/null "$1/process.php?user=batman&pass=dummy&&drop=balance" &
+    curl -s -w 'Lookup time:\t%{time_total}'\\n -o /dev/null "$1/process.php?user=$USER&pass=$PASS&&drop=balance" &
     sleep 1
 done
