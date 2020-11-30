b48c1c88bfc41c8ef17e04daf8148d400c95b95c
Paolo Coba <paolo.coba@gmail.com>
2020-11-30 10:55:20 +0100

    Fixed exploiter.py

--- secure_server/red/proxy_tunnel.sh
+++ secure_server/red/proxy_tunnel.sh
@@ -5,6 +5,6 @@ if [ "$#" -ne 3 ];then
     exit 0
 fi
 
-ssh -J $1@users.deterlab.net -D 127.0.0.1:$2 $1@$3.OffTech.isi.deterlab.net -N -f
+ssh -J $1@users.deterlab.net -D 127.0.0.1:$2 $1@$3.OffTech.isi.deterlab.net -N -f > /dev/null 2>&1
 
-chromium --proxy-server=socks://localhost:$2
+chromium --proxy-server=socks://localhost:$2 > /dev/null 2>&1
