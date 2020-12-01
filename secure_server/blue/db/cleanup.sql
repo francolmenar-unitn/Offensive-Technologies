40c3ec5c56ee839b32ce8f6d7d0f2518505a88b0
Paolo Coba <paolo.coba@gmail.com>
2020-12-01 12:55:04 +0100

    More refactoring

--- secure_server/blue/db/cleanup.sql
+++ secure_server/blue/db/cleanup.sql
@@ -1,3 +1,11 @@
+DELETE FROM users WHERE user="jelena";
+DELETE FROM users WHERE user="kate";
+DELETE FROM users WHERE user="john";
+
+DELETE FROM transfers WHERE user="jelena";
+DELETE FROM transfers WHERE user="kate";
+DELETE FROM transfers WHERE user="john";
+
 ALTER TABLE ctf2.users ENGINE=InnoDB;
 ALTER TABLE ctf2.transfers ENGINE=InnoDB;
 ALTER TABLE ctf2.transfers ADD CONSTRAINT fk_users FOREIGN KEY (user) REFERENCES ctf2.users(user);
--- secure_server/blue/setup/server_setup.sh
+++ secure_server/blue/setup/server_setup.sh
@@ -23,16 +23,23 @@ sudo mv /var/www/html/process.php /var/www/html/process.php.old
 sudo cp $TEMP_PATH/php/process_refactored.php /var/www/html/process.php
 echo "Secure php scripts successfully copied."
 
-#echo "Adding users with improved passwords"
-#echo "Adding user Jelena"
-#psw=$(./random_psw.sh)
-#curl -o /dev/null "http://127.0.0.1/process.php?user=jelena&pass=${psw}&drop=register"
-#echo "Adding user John"
-#psw=$(./random_psw.sh)
-#curl -o /dev/null "http://127.0.0.1/process.php?user=john&pass=${psw}&drop=register"
-#echo "Adding user Kate"
-#psw=$(./random_psw.sh)
-#curl -o /dev/null "http://127.0.0.1/process.php?user=kate&pass=${psw}&drop=register"
+echo "Adding users with improved passwords"
+echo "Adding user Jelena"
+psw1=$(./random_psw.sh)
+curl -o /dev/null "http://127.0.0.1/process.php?user=jelena&pass=${psw1}&drop=register"
+echo "Adding user John"
+psw2=$(./random_psw.sh)
+curl -o /dev/null "http://127.0.0.1/process.php?user=john&pass=${psw2}&drop=register"
+echo "Adding user Kate"
+psw3=$(./random_psw.sh)
+curl -o /dev/null "http://127.0.0.1/process.php?user=kate&pass=${psw3}&drop=register"
+
+echo "Adding previous transfers"
+curl -o /dev/null "http://127.0.0.1/process.php?user=jelena&pass=${psw1}&drop=deposit&amount=12345"
+curl -o /dev/null "http://127.0.0.1/process.php?user=kate&pass=${psw2}&drop=deposit&amount=98512"
+curl -o /dev/null "http://127.0.0.1/process.php?user=jelena&pass=${psw3}&drop=deposit&amount=6587"
+
+
 
 echo "Instally python3-mysql,connector module for db monitor"
 sudo apt install python3-mysql.connector
